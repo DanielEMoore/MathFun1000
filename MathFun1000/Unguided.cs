c1fc4665dd60190c82d10c30aab50aa45f61a2fc
Eric <elaib1000@gmail.com>
2015-02-08 14:22:00 -0800

    Added constructors for Fill_In and unguided classes

--- MathFun1000/Unguided.cs
+++ MathFun1000/Unguided.cs
@@ -6,5 +6,13 @@ using System.Web;
 namespace MathFun1000 {
     public class Unguided {
         public int difficulty = 3;
+        public String[] problem;
+
+        public Unguided() {
+            this.problem = new String[] { "(2x2y -3xy) + (6x2y-9xy)" };
+        }
+        public Unguided(String[] problem) {
+            this.problem = problem;
+        }
     }
 }
